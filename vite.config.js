4ce378d71791a123ae721ee242fa79d1379ed8b6
kazuhitokimura <kazuhito.k43@gmail.com>
2025-07-14 11:22:48 +0900

    リンクのパスを修正し、Viteのビルド設定を更新

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        "text-16px-p-2px": resolve(__dirname, "text-16px,p-2px.html"),
+        "text-16px-p-4px": resolve(__dirname, "text-16px,p-4px.html"),
+        "text-16px-p-6px": resolve(__dirname, "text-16px,p-6px.html"),
+        "text-16px-p-8px": resolve(__dirname, "text-16px,p-8px.html"),
+        "text-14px-p-0px": resolve(__dirname, "text-14px,p-0px.html"),
+        "text-14px-p-2px": resolve(__dirname, "text-14px,p-2px.html"),
+        "text-14px-p-4px": resolve(__dirname, "text-14px,p-4px.html"),
+        "text-14px-p-6px": resolve(__dirname, "text-14px,p-6px.html"),
+        "text-14px-p-8px": resolve(__dirname, "text-14px,p-8px.html"),
+      },
+    },
+  },
 });
